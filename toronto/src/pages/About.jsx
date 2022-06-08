1c3e44f375e8efec4a07a240de4e9b419d43b1e1
Kyubum Jang <jkb2221@gmail.com>
2022-06-08 19:13:52 +0900

    feat(Input/AboutPage): Input 컴포넌트 추가

--- toronto/src/pages/About.jsx
+++ toronto/src/pages/About.jsx
@@ -1,8 +1,11 @@
+import Input from '../components/atoms/Input/index.js'
+
 const About = () => {
   return (
     <div>
       <h1>소개</h1>
       <p>리액트 라우터를 사용해 보는 프로젝트입니다.</p>
+      <Input />
     </div>
   );
 };
---
+++ toronto/src/stories/components/atoms/Input.stories.js
@@ -0,0 +1,34 @@
+import Input from "@/components/atoms/Input";
+
+export default {
+  title: 'Component/Input',
+  component: Input,
+  argTypes: {
+    label: {
+      defaultValue: 'Label',
+      control: 'text'
+    },
+    block: {
+      defaultValue: false,
+      control: 'boolean'
+    },
+    invalid: {
+      defaultValue: false,
+      control: 'boolean'
+    },
+    required: {
+      defaultValue: false,
+      control: 'boolean'
+    },
+    disabled: {
+      defaultValue: false,
+      control: 'boolean'
+    },
+    readonly: {
+      defaultValue: false,
+      control: 'boolean'
+    }
+  }
+}
+
+export const Default = (args) => <Input {...args} />
