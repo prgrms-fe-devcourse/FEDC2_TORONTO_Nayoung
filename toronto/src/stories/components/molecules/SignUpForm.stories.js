a0570c723cc3aeea65bbce373aa015503f6e927e
GeonWooPaeng <geonwoopaeng@gmail.com>
2022-06-09 23:51:19 +0900

    feat(useForm): Add useForm

--- toronto/src/stories/components/molecules/SignUpForm.stories.js
+++ toronto/src/stories/components/molecules/SignUpForm.stories.js
@@ -3,6 +3,9 @@ import SignUpForm from '@/components/molecules/SignUpForm';
 export default {
   title: 'Component/molecules/SignUpForm',
   component: SignUpForm,
+  argTypes: {
+    onSubmit: { action: 'onSubmit' },
+  },
 };
 
 export const Default = (args) => {
