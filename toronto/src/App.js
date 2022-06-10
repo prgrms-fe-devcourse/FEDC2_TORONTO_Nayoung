670c952884187941b193e60cb94d5786f7827f41
GeonWooPaeng <geonwoopaeng@gmail.com>
2022-06-10 14:39:29 +0900

    chore(all): 안쓰는 파일 정리

--- toronto/src/App.js
+++ toronto/src/App.js
@@ -2,13 +2,15 @@ import { Routes, Route } from 'react-router-dom';
 import Layout from './layout/Layout';
 import Home from './pages/Home';
 import About from './pages/About';
+import SignUp from './pages/SignUp';
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<Layout />}>
+      <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path='/about' element={<About />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signup" element={<SignUp />} />
       </Route>
     </Routes>
   );
---
+++ toronto/src/pages/SignUp.jsx
@@ -0,0 +1,7 @@
+import SignUpForm from '@/components/molecules/SignUpForm';
+
+const SignUp = () => {
+  return <SignUpForm />;
+};
+
+export default SignUp;
