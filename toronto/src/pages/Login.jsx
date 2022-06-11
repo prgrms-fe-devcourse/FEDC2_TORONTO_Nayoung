5769338308f5fbdc6db608e97ed3dc2d0cf1cd0b
GeonWooPaeng <geonwoopaeng@gmail.com>
2022-06-12 01:38:06 +0900

    feat(Login): Login Wrapper 추가

--- toronto/src/pages/Login.jsx
+++ toronto/src/pages/Login.jsx
@@ -1,7 +1,21 @@
+import styled from 'styled-components';
 import LoginForm from '@/components/molecules/LoginForm';
 
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #f9fafb;
+`;
+
 const Login = () => {
-  return <LoginForm />;
+  return (
+    <Wrapper>
+      <LoginForm />
+    </Wrapper>
+  );
 };
 
 export default Login;
