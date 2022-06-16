c07fdf2d1415d8616a82610d41c140c5113a414b
GeonWooPaeng <geonwoopaeng@gmail.com>
2022-06-16 16:55:50 +0900

    feat(controversyVoteForm): 논쟁 페이지 기능 구현

--- toronto/src/components/molecules/ControversyVote/index.js
+++ toronto/src/components/molecules/ControversyVote/index.js
@@ -6,7 +6,7 @@ import { useEffect, useState } from 'react';
 
 const ControversyVoteWrapper = styled.div`
   display: flex;
-  justify-content: space-evenly;
+  justify-content: space-between;
   align-items: center;
 `;
 
