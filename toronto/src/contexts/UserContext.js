4bf375f19046c33eb6972d488eb9ec3c356bf7e5
Kyubum Jang <jkb2221@gmail.com>
2022-06-17 00:50:45 +0900

    feat(EditProfile): 프로필 수정 기능 구현

--- toronto/src/contexts/UserContext.js
+++ toronto/src/contexts/UserContext.js
@@ -1,10 +1,15 @@
 import React, { createContext, useReducer, useContext } from 'react';
-import { getUsersApi, getUserApi, postLoginApi } from '@/api/Api';
+import {
+  getUsersApi,
+  getUserApi,
+  postLoginApi,
+  postUploadPhotoApi,
+} from '@/api/Api';
 import {
   createAsyncHandler,
   initialAsyncState,
 } from '@/utils/asyncActionUtils';
-import { getAuthUser } from '../api/Api';
+import { getAuthUser, putUpdateUserApi } from '../api/Api';
 //TODO: 새로고침해도 쿠키가 있을경우 로그인 상태로 유지, get auth  => cookie 동작
 //TODO: 로그아웃 시 쿠키 삭제
 
@@ -17,6 +22,12 @@ const usersHandler = createAsyncHandler('GET_USERS', 'users');
 const userHandler = createAsyncHandler('GET_USER', 'user');
 const loginHandler = createAsyncHandler('POST_LOGIN', 'user');
 const authHandler = createAsyncHandler('GET_AUTH_USER', 'user');
+const updateHandler = createAsyncHandler('PUT_UPDATE_USER', 'user');
+const uploadProfileImageHandler = createAsyncHandler(
+  'POST_PROFILE_IMAGE',
+  'user',
+);
+
 const usersReducer = (state, action) => {
   switch (action.type) {
     case 'GET_USERS':
@@ -35,6 +46,15 @@ const usersReducer = (state, action) => {
     case 'GET_AUTH_USER_SUCCESS':
     case 'GET_AUTH_USER_ERROR':
       return authHandler(state, action);
+    case 'PUT_UPDATE_USER':
+    case 'PUT_UPDATE_USER_SUCCESS':
+    case 'PUT_UPDATE_USER_ERROR':
+      return updateHandler(state, action);
+    case 'POST_PROFILE_IMAGE':
+    case 'POST_PROFILE_IMAGE_SUCCESS':
+    case 'POST_PROFILE_IMAGE_ERROR':
+      return uploadProfileImageHandler(state, action);
+
     default:
       throw new Error(`Unhanded action type: ${action.type}`);
   }
@@ -116,5 +136,27 @@ export async function getAuth(dispatch) {
     dispatch({ type: 'GET_AUTH_USER_SUCCESS', data: response.data });
   } catch (e) {
     dispatch({ type: 'GET_AUTH_USER_ERROR', error: e });
+  }
+}
+
+export async function putUpdateUser(dispatch, fullName, username) {
+  dispatch({ type: 'PUT_UPDATE_USER' });
+  try {
+    const response = await putUpdateUserApi(fullName, username);
+    dispatch({ type: 'PUT_UPDATE_USER_SUCCESS', data: response.data });
+    alert('성명이 성공적으로 바뀌었습니다!');
+  } catch (e) {
+    dispatch({ type: 'PUT_UPDATE_USER_ERROR', error: e });
+  }
+}
+
+export async function postProfileImage(dispatch, bodyFormData) {
+  dispatch({ type: 'POST_PROFILE_IMAGE' });
+  try {
+    const response = await postUploadPhotoApi(bodyFormData);
+    dispatch({ type: 'POST_PROFILE_IMAGE_SUCCESS', data: response.data });
+    alert('이미지가 성공적으로 바뀌었습니다!');
+  } catch (e) {
+    dispatch({ type: 'POST_PROFILE_IMAGE_ERROR', error: e });
   }
 }
