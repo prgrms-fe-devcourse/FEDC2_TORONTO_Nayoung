3a728504209fa5cb857ca2ef6a9834a986f6b7d7
Kyubum Jang <jkb2221@gmail.com>
2022-06-18 02:12:52 +0900

    feat(Api): 채널 Api 추가

--- toronto/src/api/Api.js
+++ toronto/src/api/Api.js
@@ -7,7 +7,7 @@ export const postLoginApi = async ({ email, password }) => {
     email,
     password,
   });
-  onSaveToken(res.data.token); //save token in cookie
+  onSaveToken(res.data.token);
   return res;
 };
 
@@ -49,7 +49,6 @@ export const getUserApi = async (userId) => {
 // 프로필 이미지 변경 및 커버 이미지 변경
 export const postUploadPhotoApi = async (bodyFormData) => {
   const token = getToken();
-  console.log(token);
   const res = await axios({
     method: 'post',
     url: `${process.env.REACT_APP_END_POINT}/users/upload-photo`,
@@ -80,3 +79,33 @@ export const putUpdatePasswordApi = async (password) => {
 };
 
 // TODO: 채널부터 추가
+
+// EditProfile 내 게시물 임시 더미데이터 API
+export const getUserPostApi = async () => {
+  const res = await axios.get('/data/postDummy.json');
+  return res;
+};
+
+// EditProfile 좋아요 게시물 임시 더미데이터 API
+export const getUserDummyApi = async () => {
+  const res = await axios.get('/data/userDummy.json');
+  return res;
+};
+
+// 채널 목록 불러오기
+export const getChannels = async () => {
+  const res = await Send.get('/channels');
+  return res;
+};
+
+// 특정 채널 목록 불러오기
+export const getChannelsName = async (channelName) => {
+  const res = await Send.get(`/channels/${channelName}`);
+  return res;
+};
+
+// 특정 채널의 포스트 목록
+export const getPostsChannel = async (channelId) => {
+  const res = await Send.get(`posts/channel/${channelId}`);
+  return res;
+};
