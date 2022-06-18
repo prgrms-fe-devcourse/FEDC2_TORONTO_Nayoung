8751ca670ffc815576ca002927662d10003b063e
qq8721443 <qq8721443@naver.com>
2022-06-18 16:27:39 +0900

    chore(Components): index.js 수정

--- toronto/src/components/atoms/index.js
+++ toronto/src/components/atoms/index.js
@@ -2,6 +2,7 @@ export { default as Avatar } from './Avatar';
 export { default as Button } from './Button';
 export { default as Card } from './Card';
 export { default as Divider } from './Divider';
+export { default as DoughnutChart } from './DoughnutChart';
 export { default as Header } from './Header';
 export { default as Icon } from './Icon';
 export { default as Image } from './Image';
