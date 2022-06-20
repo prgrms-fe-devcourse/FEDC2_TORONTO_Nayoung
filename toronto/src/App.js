f5cc061f855c8b4b26635f7fabde621c7edbf3ab
Kyubum Jang <jkb2221@gmail.com>
2022-06-21 02:19:52 +0900

    chore(mui): mui 적용

--- toronto/src/App.js
+++ toronto/src/App.js
@@ -14,26 +14,38 @@ import {
 } from '@pages';
 import UsersProvider from './contexts/UserContext';
 import UserListPage from './pages/UserListPage';
+import { createTheme, ThemeProvider } from '@mui/material';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: 'Roboto',
+  },
+});
 
 function App() {
   return (
-    <UsersProvider>
-      <Routes>
-        <Route path='/' element={<Layout />}>
-          <Route index element={<HomePage />} />
-          <Route path='/about' element={<AboutPage />} />
-          <Route path='/create-post' element={<PostPage />} />
-          <Route path='/users/:userId' element={<UserProfilePage />} />
-          <Route path='/users/:userId/edit' element={<EditProfilePage />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/sign-up' element={<SignUp />} />
-          <Route path='/controversy/result/:postId' element={<ResultPage />} />
-          <Route path='/controversy/:postId' element={<Controversy />} />
-          <Route path='/user/list' element={<UserListPage />} />
-        </Route>
-        <Route path='*' element={<NotFoundPage />} />
-      </Routes>
-    </UsersProvider>
+    <ThemeProvider theme={theme}>
+      <UsersProvider>
+        <Routes>
+          <Route path='/' element={<Layout />}>
+            <Route index element={<HomePage />} />
+            <Route path='/about' element={<AboutPage />} />
+            <Route path='/create-post' element={<PostPage />} />
+            <Route path='/users/:userId' element={<UserProfilePage />} />
+            <Route path='/users/:userId/edit' element={<EditProfilePage />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/sign-up' element={<SignUp />} />
+            <Route
+              path='/controversy/result/:postId'
+              element={<ResultPage />}
+            />
+            <Route path='/controversy/:postId' element={<Controversy />} />
+            <Route path='/user/list' element={<UserListPage />} />
+          </Route>
+          <Route path='*' element={<NotFoundPage />} />
+        </Routes>
+      </UsersProvider>
+    </ThemeProvider>
   );
 }
 
