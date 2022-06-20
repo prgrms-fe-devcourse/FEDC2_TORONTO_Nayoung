fa3aa8f64b683fddfe0e5cc4736b6ca529f684bb
DESKTOP-H8C5V9L\ekzm5 <jaeung5169@naver.com>
2022-06-20 15:47:24 +0900

    remove(PostItem): PostItem 에서 좋아요 기능 삭제

--- toronto/src/components/molecules/PostItem/index.js
+++ toronto/src/components/molecules/PostItem/index.js
@@ -3,13 +3,8 @@ import Card from '@/components/atoms/Card';
 import Header from '@/components/atoms/Header';
 import Image from '@/components/atoms/Image';
 import Text from '@/components/atoms/Text';
-import Icon from '@/components/atoms/Icon';
-import Loader from '@/components/atoms/Loader';
-import axios from 'axios';
 import styled from 'styled-components';
-import { useState } from 'react';
-import { useUsersState } from '@/contexts/UserContext';
-import { getToken } from '@/lib/Login';
+// import { useUsersState } from '@/contexts/UserContext';
 
 const StyledLi = styled.li`
   list-style: none;
@@ -32,65 +27,12 @@ const ContentContainer = styled.div`
   flex-direction: column;
 `;
 
-const StyledButton = styled.button`
-  border: none;
-  background-color: transparent;
-  cursor: pointer;
-  padding: 0;
-
-  &:hover {
-    transform: scale(1.2);
-  }
-`;
-
 const PostItem = ({ post }) => {
-  const state = useUsersState();
-  const { data: user } = state.user;
-  const userId = user?._id;
-  const { _id: postId, image, likes } = post;
+  // const state = useUsersState();
+  // const { data: user } = state.user;
+  // const userId = user?._id;
+  const { _id: postId, image } = post;
   const { postTitle, postContent } = JSON.parse(post.title);
-  const like = likes
-    ? likes.filter(({ user }) => user === userId)[0]
-    : undefined;
-  const [isLike, setIsLike] = useState(Boolean(like));
-  const [isLoading, setIsLoading] = useState(false);
-  const [likeId, setLikeId] = useState(like?._id);
-
-  const handleClick = async () => {
-    if (!userId) {
-      alert('좋아요는 로그인 한 사용자만 누를 수 있습니다.');
-      return;
-    }
-    setIsLoading(true);
-
-    if (!isLike) {
-      const res = await axios.post(
-        `${process.env.REACT_APP_END_POINT}/likes/create`,
-        {
-          postId: postId,
-        },
-        {
-          headers: {
-            Authorization: `bearer ${getToken()}`,
-          },
-        },
-      );
-
-      setLikeId(res.data._id);
-    } else {
-      await axios.delete(`${process.env.REACT_APP_END_POINT}/likes/delete`, {
-        headers: {
-          Authorization: `bearer ${getToken()}`,
-        },
-        data: {
-          id: likeId,
-        },
-      });
-    }
-
-    setIsLike((like) => !like);
-    setIsLoading(false);
-  };
 
   return (
     <StyledLi>
@@ -112,21 +54,6 @@ const PostItem = ({ post }) => {
             >
               {postTitle}
             </Header>
-            {!isLoading ? (
-              <StyledButton onClick={handleClick}>
-                {isLike ? (
-                  <Icon
-                    iconName={'thumbs-up'}
-                    strokeWidth={1.5}
-                    fill={'#2366F6'}
-                  />
-                ) : (
-                  <Icon iconName={'thumbs-up'} strokeWidth={1.5} />
-                )}
-              </StyledButton>
-            ) : (
-              <Loader size={24} loading={isLoading} />
-            )}
           </TitleContainer>
           <StyledLink to={`/controversy/${postId}`} style={{ color: 'black' }}>
             <ContentContainer>
