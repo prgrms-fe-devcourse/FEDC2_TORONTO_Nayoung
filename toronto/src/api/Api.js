a2285aebee9d15d07f8b39a568b134d90aee8899
qq8721443 <qq8721443@naver.com>
2022-06-21 17:37:19 +0900

    refactor(Api.js): 서버리스 함수 formData 제외 완성

--- toronto/src/api/Api.js
+++ toronto/src/api/Api.js
@@ -1,4 +1,4 @@
-// import axios from 'axios';
+import axios from 'axios';
 import Send from '.';
 import { getToken, onSaveToken } from '@/lib/Login.js';
 
@@ -98,53 +98,33 @@ export const getUserApi = async (userId) => {
 
 // 프로필 이미지 변경 및 커버 이미지 변경
 export const postUploadPhotoApi = async (bodyFormData) => {
-  const res = await serverlessFormData('/users/upload-photo', bodyFormData);
-  return res.data;
-  // 파일 변환 방식 미완성
-  // const obj = {};
-  // bodyFormData.forEach((value, key) => (obj[key] = value));
-  // const reader = new FileReader(obj.image);
-  // let result;
-  // reader.onload = async () => {
-  //   result = reader.result;
-  //   console.log('onload! ' + result);
-  //   const token = getToken();
-  //   const res = await serverless({
-  //     method: 'POST',
-  //     url: `/users/upload-photo`,
-  //     headers: {
-  //       'Content-Type': 'multipart/form-data',
-  //       Authorization: `Bearer ${token}`,
-  //     },
-  //     body: JSON.stringify({
-  //       ...obj,
-  //       image: result,
-  //     }),
-  //   });
-  //   console.log(res);
-  // };
+  const token = getToken();
   // 원래 코드
-  // const res = await axios({
-  //   method: 'post',
-  //   url: `${process.env.REACT_APP_END_POINT}/users/upload-photo`,
-  //   data: bodyFormData,
-  //   headers: {
-  //     'Content-Type': 'multipart/form-data',
-  //     Authorization: `Bearer ${token}`,
-  //   },
-  // });
-  // return res.data;
+  const res = await axios({
+    method: 'post',
+    url: `${process.env.REACT_APP_END_POINT}/users/upload-photo`,
+    data: bodyFormData,
+    headers: {
+      'Content-Type': 'multipart/form-data',
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return res.data;
 };
 
 // 내 정보 변경
 export const putUpdateUserApi = async (fullName, username) => {
-  // 동작은 잘 하고 프론트 url이 잘못된듯?
+  // 요청은 잘 되고 프론트 url 변경해야 할 듯?
+  const token = getToken();
   const res = await serverless({
     method: 'PUT',
     url: '/settings/update-user',
     body: {
       fullName,
       username,
+    },
+    headers: {
+      Authorization: `Bearer ${token}`,
     },
   });
   return res.data;
@@ -167,13 +147,12 @@ export const putUpdatePasswordApi = async (password) => {
   return res;
 };
 
-// 전체 posts 목록 불러오기 필요없음
+// 전체 posts 목록 불러오기
 export const getPostsApi = async () => {
   const res = await serverless({
     method: 'GET',
     url: '/posts',
   });
-  // const res = await Send.get('/posts');
   return res;
 };
 
@@ -183,7 +162,6 @@ export const getChannels = async () => {
     method: 'GET',
     url: '/channels',
   });
-  // const res = await Send.get('/channels');
   return res;
 };
 
@@ -193,13 +171,11 @@ export const getChannelsName = async (channelName) => {
     method: 'GET',
     url: `/channels/${channelName}`,
   });
-  // const res = await Send.get(`/channels/${channelName}`);
   return res;
 };
 
 // 특정 채널의 포스트 목록
 export const getPostsChannel = async (channelId) => {
-  // const res = await Send.get(`posts/channel/${channelId}`);
   const res = await serverless({
     method: 'GET',
     url: `posts/channel/${channelId}`,
@@ -303,23 +279,6 @@ export const serverless = async (options) => {
     const res = await fetch(`/.netlify/functions/request`, {
       method: 'POST',
       body: JSON.stringify(options),
-    });
-    return await res.json();
-  } catch (e) {
-    console.log('serverless에서 request로 넘어갈 때 오류남 ' + e);
-  }
-};
-
-export const serverlessFormData = async (url, formData) => {
-  const token = getToken();
-  try {
-    const res = await fetch(`/.netlify/functions/request${url}`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'multipart/form-data',
-        Authorization: `Bearer ${token}`,
-      },
-      body: formData,
     });
     return await res.json();
   } catch (e) {
