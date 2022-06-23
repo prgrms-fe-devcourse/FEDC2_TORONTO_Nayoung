2a9d123b4ebeac8f6eafffebe9b91b27dba8083c
Kyubum Jang <jkb2221@gmail.com>
2022-06-23 17:32:15 +0900

    feat(Skeleton): Notification 스켈레톤 추가

--- toronto/src/components/atoms/Skeleton/index.js
+++ toronto/src/components/atoms/Skeleton/index.js
@@ -1,11 +1,12 @@
 import Box from './Box';
 import Circle from './Circle';
 import Paragraph from './Paragraph';
-
+import Notification from './Notification';
 const Skeleton = {
   Box,
   Circle,
   Paragraph,
+  Notification,
 };
 
 export default Skeleton;
--- toronto/src/pages/Notifications.jsx
+++ toronto/src/pages/Notifications.jsx
@@ -1,47 +1,63 @@
+/* eslint-disable no-unused-vars */
 import styled from 'styled-components';
-import { Header, Icon } from '@/components/atoms';
+import { Header, Skeleton } from '@/components/atoms';
 import Notification from '@/components/molecules/Notification';
 import { useEffect, useState } from 'react';
-import { getNotifications } from '@/api/Api';
+import { getNotifications, getPostsApi } from '@/api/Api';
 const Notifications = () => {
-  // API 연동
   const [notifications, setNotifications] = useState([]);
+  const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   useEffect(() => {
     const fetchData = async () => {
+      setIsLoading(true);
       const response = await getNotifications();
+      const postsRes = await getPostsApi();
+      setPosts(postsRes.data);
       setNotifications(response.data);
+      setIsLoading(false);
     };
     fetchData();
   }, []);
+
+  const renderSkeleton = () => {
+    const skeletons = [];
+    for (let i = 0; i < 3; i++) {
+      skeletons.push(
+        <Skeleton.Notification key={i} width={'100%'} height={'100%'} />,
+      );
+    }
+    return skeletons;
+  };
 
   return (
     <ContentWrapper>
       <Wrapper>
         <Section>
           <Header level={2}>알림</Header>
-          <StyledButton>
-            <Icon
-              size={20}
-              iconName='more-horizontal'
-              style={{ verticalAlign: 'bottom' }}
-            />
-          </StyledButton>
         </Section>
         <Section>
           <ColWrapper>
-            <Header level={4}>새로운 알림</Header>
-            <ColWrapper>
+            {notifications.filter((notification) => notification.seen === false)
+              .length >= 1 ? (
+              <Header level={4}>새로운 알림</Header>
+            ) : (
+              ''
+            )}
+            <NotificationWrapper>
+              {isLoading && renderSkeleton()}
               {notifications ? (
                 notifications.map((data) => (
                   <Notification
                     key={data._id}
                     notification={data}
+                    posts={posts}
                   ></Notification>
                 ))
               ) : (
                 <div></div>
               )}
-            </ColWrapper>
+            </NotificationWrapper>
           </ColWrapper>
         </Section>
       </Wrapper>
@@ -53,10 +69,9 @@ export default Notifications;
 
 const ContentWrapper = styled.div`
   display: flex;
+  height: 100%;
   align-items: center;
   justify-content: center;
-  height: 80vh;
-  background-color: #f9fafb;
 `;
 
 const Wrapper = styled.div`
@@ -64,41 +79,25 @@ const Wrapper = styled.div`
   align-items: center;
   flex-direction: column;
   background-color: #fff;
-  width: 700px;
-  height: 700px;
+  width: 100%;
+  height: 100%;
 `;
 
 const Section = styled.section`
   display: flex;
-  width: 90%;
   align-items: center;
   justify-content: space-between;
+  margin: 1rem 0 auto;
 `;
 
 const ColWrapper = styled.div`
   display: flex;
   flex-direction: column;
+  gap: 1rem;
 `;
 
-const StyledButton = styled.button`
-  outline: none;
-  border: none;
-  cursor: pointer;
-  background-color: #fff;
-  color: white;
-  padding: 10px;
-  border-radius: 4px;
+const NotificationWrapper = styled.div`
   display: flex;
-  align-items: center;
-  justify-content: center;
-  &:hover {
-    background-color: #eeeeee;
-  }
-  &:active {
-    background-color: #f9fafb;
-  }
-  &:disabled {
-    background-color: gray;
-    cursor: default;
-  }
+  flex-direction: column;
+  border-radius: 4px;
 `;
